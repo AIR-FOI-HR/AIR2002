d178fe8ae9ef222e04abb1dda29590a9dede1dd6
Josip Bezi <josip.bezi@gmail.com>
2020-11-04 18:56:09 +0100

    Added Question method, connected with trivia api.

---
+++ Trivia.ly-Services/Controllers/QuestionController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Trivia.ly_Services.Models;
+
+namespace Trivia.ly_Services.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class QuestionController : ControllerBase
+    {
+
+        [HttpGet]
+        public Question Get()
+        {
+            return GetQuestions();
+        }
+
+        public Question GetQuestions()
+        {
+            var response = new WebClient().DownloadString("https://opentdb.com/api.php?amount=10&type=multiple");
+
+            return JsonSerializer.Deserialize<Question>(response);
+        }
+    }
+}
---
+++ Trivia.ly-Services/Models/Question.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Trivia.ly_Services.Models
+{
+    public class Result
+    {
+        public string category { get; set; }
+        public string type { get; set; }
+        public string difficulty { get; set; }
+        public string question { get; set; }
+        public string correct_answer { get; set; }
+        public List<string> incorrect_answers { get; set; }
+    }
+
+    public class Question
+    {
+        public int response_code { get; set; }
+        public List<Result> results { get; set; }
+    }
+}

50e0f5623c92db68efaece1046f358eae098bd9f
Josip Bezi <josip.bezi@gmail.com>
2020-11-19 22:52:04 +0100

    Spajanje na bazu in dev

---
+++ Trivia.ly-Services/Data/TrivialyContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Trivia.ly_Services.Models;
+
+namespace Trivia.ly_Services.Data
+{
+    public class TrivialyContext : DbContext
+    {
+        public TrivialyContext(DbContextOptions options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}

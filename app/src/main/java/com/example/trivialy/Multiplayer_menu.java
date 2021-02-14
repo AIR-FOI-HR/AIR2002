c0bf430100b9c2d2f31b65f7aa2a60219e2f0975
LidSalopek <lidsalope@foi.hr>
2021-02-14 16:07:12 +0100

    Added activity for multiplayer menu

---
+++ app/src/main/java/com/example/trivialy/Multiplayer_menu.java
@@ -0,0 +1,15 @@
+package com.example.trivialy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Multiplayer_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.multiplayer_menu);
+    }
+}

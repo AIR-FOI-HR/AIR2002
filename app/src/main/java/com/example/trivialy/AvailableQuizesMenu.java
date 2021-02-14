d1b082a424a38d3b40d947f0dafd157910e7261f
LidSalopek <lidsalope@foi.hr>
2021-02-14 17:44:19 +0100

    created category menu for multiplayer

---
+++ app/src/main/java/com/example/trivialy/AvailableQuizesMenu.java
@@ -0,0 +1,14 @@
+package com.example.trivialy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailableQuizesMenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.available_quizes_menu);
+    }
+}
